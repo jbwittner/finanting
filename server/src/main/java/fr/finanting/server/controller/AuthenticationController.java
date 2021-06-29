32cd81f8a4e6b1d171c279bc7af5ab4ace61ea7c
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-06-30 00:51:39 +0200

    update

--- server/src/main/java/fr/finanting/server/controller/AuthenticationController.java
+++ server/src/main/java/fr/finanting/server/controller/AuthenticationController.java
@@ -1,15 +1,10 @@
 package fr.finanting.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.finanting.server.exception.LoginException;
 import fr.finanting.server.generated.api.AuthenticationApi;
 import fr.finanting.server.generated.model.LoginDTO;
 import fr.finanting.server.generated.model.LoginParameter;
@@ -19,15 +14,12 @@ import fr.finanting.server.security.JwtTokenUtil;
 public class AuthenticationController extends MotherController implements AuthenticationApi {
     
     @Autowired
-    private AuthenticationManager authenticationManager;
- 
-    @Autowired
     private JwtTokenUtil jwtUtil;
 
     @Override
     public ResponseEntity<LoginDTO> login(final LoginParameter loginParameter) {
         
-        String jwtToken = this.getToken(loginParameter);
+        String jwtToken = jwtUtil.getToken(loginParameter);
 
         LoginDTO loginDTO = new LoginDTO();
         loginDTO.setJwt(jwtToken);
@@ -35,25 +27,5 @@ public class AuthenticationController extends MotherController implements Authen
         return new ResponseEntity<>(loginDTO, HttpStatus.OK);
 
     }
-
-    private String getToken(final LoginParameter loginParameter) {
-        
-        UsernamePasswordAuthenticationToken loginCredentials =
-                new UsernamePasswordAuthenticationToken(
-                        loginParameter.getUserName(), loginParameter.getPassword());
-
-        Authentication authentication;
-
-        try {
-            authentication = 
-                authenticationManager.authenticate(loginCredentials);
-        } catch (Exception e) {
-               throw new LoginException(e);
-        }
- 
-        String userName = authentication.getName();
-        String jwtToken = jwtUtil.createJWT(userName);
- 
-        return jwtToken;
-    }
+   
 }
---
+++ server/src/main/java/fr/finanting/server/security/implementation/JwtTokenUtilImpl.java
@@ -0,0 +1,140 @@
+package fr.finanting.server.security.implementation;
+
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import fr.finanting.server.exception.LoginException;
+import fr.finanting.server.generated.model.LoginParameter;
+import fr.finanting.server.model.User;
+import fr.finanting.server.repository.UserRepository;
+import fr.finanting.server.security.JwtTokenUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenUtilImpl implements JwtTokenUtil {
+
+    @Value("${application.jwt.issuer}")
+    private String issuer;
+ 
+    @Value("${application.jwt.secret}")
+    private String secret;
+ 
+    @Value("${application.jwt.audience}")
+    private String audience;
+ 
+    @Value("${application.jwt.ttl-in-seconds}")
+    private long timeToLiveInSeconds;
+
+    private SecretKey secretKey;
+
+    private final Logger logger = LoggerFactory.getLogger(JwtTokenUtil.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private static final String CLAIM_FIRST_NAME_KEY = "FirstName";
+    private static final String CLAIM_LAST_NAME_KEY = "LastName";
+
+    @PostConstruct
+    private void setUpSecretKey() {
+        try {
+            secretKey = Keys.hmacShaKeyFor(secret.getBytes("UTF-8"));
+            logger.info("Secret key generation ok !");
+        } catch (UnsupportedEncodingException e) {
+            logger.error("Error generating JWT Secret Key : {}", e.getMessage());
+            throw new RuntimeException("Error generating JWT Secret Key", e);
+        }
+    }
+
+    private String createJWT(String userName) {
+
+        User user = userRepository.findByUserName(userName).orElseThrow();
+        
+        Date issuedAt = Date.from(Instant.now());
+        Date expiration = Date.from(Instant.now().plus(Duration.ofSeconds(this.timeToLiveInSeconds)));
+
+        String randomUUID = UUID.randomUUID().toString();
+
+        String pattern = "dd-MM-yyyy HH:mm:ss";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+        this.logger.info("Generation of new JWT [username : " + userName +
+            " / randomUUID : " + randomUUID +
+            " / issuedAt : " + simpleDateFormat.format(issuedAt) +
+            " / expiration : " + simpleDateFormat.format(expiration)
+            + "]");
+ 
+        String jwt =
+            Jwts.builder()
+                .setId(randomUUID)
+                .setSubject(user.getUserName())
+                .setIssuer(this.issuer)
+                .setAudience(this.audience)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .claim(CLAIM_FIRST_NAME_KEY, user.getFirstName())
+                .claim(CLAIM_LAST_NAME_KEY, user.getLastName())
+                .signWith(secretKey)
+                .compact();
+
+        return jwt;
+    }
+
+    public Jws<Claims> parseJWT(String jwtString) {
+ 
+        Jws<Claims> claims =
+            Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(jwtString);
+     
+        return claims;
+    }
+
+    public String getToken(final LoginParameter loginParameter){
+
+        UsernamePasswordAuthenticationToken loginCredentials =
+                new UsernamePasswordAuthenticationToken(
+                        loginParameter.getUserName(), loginParameter.getPassword());
+
+        Authentication authentication;
+
+        try {
+            authentication = 
+                authenticationManager.authenticate(loginCredentials);
+        } catch (Exception e) {
+               throw new LoginException(e);
+        }
+ 
+        String userName = authentication.getName();
+        String jwtToken = this.createJWT(userName);
+ 
+        return jwtToken;
+
+    }
+    
+}
