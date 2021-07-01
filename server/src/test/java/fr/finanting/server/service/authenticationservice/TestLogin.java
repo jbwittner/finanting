5c260aae4b505d10edbec93f451c8d290257f891
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-07-01 22:02:22 +0200

    Coverage ok

--- server/src/test/java/fr/finanting/server/service/authenticationservice/TestLogin.java
+++ server/src/test/java/fr/finanting/server/service/authenticationservice/TestLogin.java
@@ -8,8 +8,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 
 import fr.finanting.server.exception.LoginException;
 import fr.finanting.server.generated.model.LoginParameter;
+import fr.finanting.server.generated.model.UserDTO;
+import fr.finanting.server.generated.model.UserRegistrationParameter;
 import fr.finanting.server.model.User;
 import fr.finanting.server.security.JwtTokenUtil;
+import fr.finanting.server.service.UserService;
 import fr.finanting.server.service.implementation.AuthenticationServiceImpl;
 import fr.finanting.server.testhelper.AbstractMotherIntegrationTest;
 
@@ -21,27 +24,43 @@ public class TestLogin extends AbstractMotherIntegrationTest {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserService userService;
+
     private AuthenticationServiceImpl authenticationServiceImpl;
+
+    private LoginParameter loginParameter;
 
     @Override
     protected void initDataBeforeEach() {
-        this.authenticationServiceImpl = new AuthenticationServiceImpl(this.jwtTokenUtil, this.authenticationManager);       
+        this.authenticationServiceImpl = new AuthenticationServiceImpl(this.jwtTokenUtil, this.authenticationManager);
+        UserRegistrationParameter userRegistrationParameter = new UserRegistrationParameter();
+        userRegistrationParameter.setEmail(this.testFactory.getUniqueRandomEmail());
+        userRegistrationParameter.setFirstName(this.testFactory.getRandomAlphanumericString());
+        userRegistrationParameter.setLastName(this.testFactory.getRandomAlphanumericString());
+        userRegistrationParameter.setUserName(this.testFactory.getRandomAlphanumericString());
+        String password = this.testFactory.getRandomAlphanumericString();
+        userRegistrationParameter.setPassword(password);
+        UserDTO userDTO = this.userService.registerNewAccount(userRegistrationParameter);
+        loginParameter = new LoginParameter();
+        loginParameter.setUserName(userDTO.getUserName());
+        loginParameter.setPassword(password);
     }
 
     @Test
     public void testUserNotExist(){
-        LoginParameter loginParameter = new LoginParameter();
         loginParameter.setUserName(this.testFactory.getRandomAlphanumericString());
-        loginParameter.setPassword(this.testFactory.getRandomAlphanumericString());
         Assertions.assertThrows(LoginException.class, () -> this.authenticationServiceImpl.login(loginParameter));
     }
 
     @Test
-    public void testUserNotExistdd(){
-        User user = this.testFactory.getUser();
-        LoginParameter loginParameter = new LoginParameter();
-        loginParameter.setUserName(user.getUserName());
-        loginParameter.setPassword(user.getPassword());
+    public void testUserAuthenticationOk(){
+        this.authenticationServiceImpl.login(loginParameter);
+    }
+
+    @Test
+    public void testBadPassword(){
+        loginParameter.setPassword(this.testFactory.getRandomAlphanumericString());
         Assertions.assertThrows(LoginException.class, () -> this.authenticationServiceImpl.login(loginParameter));
     }
     
