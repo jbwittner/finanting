3aec28e01088506cef24ed28e9f1edb90c5655a1
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2021-07-08 01:00:17 +0200

    adding react hook form and try to log

--- frontend/src/app/service/login/loginpage.tsx
+++ frontend/src/app/service/login/loginpage.tsx
@@ -1,10 +1,62 @@
-import { TextField } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
+import { AxiosResponse } from "axios";
 import React from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { AuthenticationApi, LoginDTO, LoginParameter } from "../../../generated";
+
+const style = {
+    divStyle : {
+        display: 'flex',
+        flexdirection : 'column',
+        margin: '4px'
+    }
+}
+
+export interface exceptionDTO {
+    details: string;
+    exception: string;
+    message: string;
+    timestamp: string;
+}
 
 export const LoginPage = () => {
 
-    return <React.Fragment>
-        <TextField id="outlined-basic" label="Username" variant="outlined" />
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
-    </React.Fragment>
+    const api: AuthenticationApi = new AuthenticationApi()
+
+    const { handleSubmit, control, formState: { errors }} = useForm<LoginParameter>();
+
+    const onSubmit: SubmitHandler<LoginParameter> = (data: LoginParameter) => {
+        console.log(data)
+
+        api.login(data)
+        .then((out:AxiosResponse<LoginDTO>) => {
+            const loginDTO:LoginDTO = out.data;
+            console.log(loginDTO)
+        })
+        .catch((error) => {
+            const exceptionDTOddd: exceptionDTO = error.response.data;
+            console.log(exceptionDTOddd)
+            console.log(exceptionDTOddd.timestamp)
+            const datess = new Date(exceptionDTOddd.timestamp)
+            console.log(datess.getFullYear())
+        })
+    }
+
+    return <div style={style.divStyle}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                name="userName"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => <TextField error={errors.userName !== undefined} id="outlined-basic" label="Username" variant="outlined" {...field} />}
+            />
+            <Controller
+                name="password"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => <TextField error={errors.password !== undefined} id="outlined-basic" label="Password" variant="outlined" {...field} />}
+            />
+            <Button variant="contained" type="submit">Default</Button>
+        </form>
+    </div>
 }
